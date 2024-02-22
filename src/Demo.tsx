6837d676847334c86b2742a6220a5315bea75987
xlaoru <sia212007@gmail.com>
2024-02-22 14:24:55 +0200

    v1.3

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -9,24 +9,37 @@ interface CodeBlockProps {
 }
 
 const CodeBlock: React.FC<CodeBlockProps> = ({ language, value }) => {
-  return <SyntaxHighlighter language={language} style={atomOneDark}>{value}</SyntaxHighlighter>;
+  return <div style={{maxWidth: '300px'}}><SyntaxHighlighter language={language} style={atomOneDark}>{value}</SyntaxHighlighter></div>
 };
 
 const Demo: React.FC = () => {
   const markdownContent = `
-### Let's create content using Markdown + CodeBlock
+# Let's create content using Markdown + CodeBlock
+
+## Here is a javascript example
+
+It's a simple example of using console for loggin string in javascript
 
 \`\`\`javascript
 console.log("Hello")
 \`\`\`
 
+## Here is a typescript example
+
+It's a simple example of declaring variable in typescript using number type
+
 \`\`\`typescript
 const a: number = 1
 \`\`\`
+
+# Here is TS logo
+![TS Logo](https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png)
 `;
 
   return (
-    <div>
+    <div style={{
+
+    }}>
       <ReactMarkdown
         components={{
           code: ({ node, children, ...props }) => {
@@ -34,6 +47,7 @@ const a: number = 1
             const language = className.replace('language-', '');
             return <CodeBlock language={language} value={String(children).replace(/\n$/, '')} />;
           },
+          img: ({ node, ...props }) => <img {...props} style={{width: '150px', height: '150px'}} />
         }}
       >
         {markdownContent}
